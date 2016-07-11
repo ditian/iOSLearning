9698647a069f63f921aeaf51a6ae822ec35db104
Di Tian <peacefultd@gmail.com>
2016-07-11 15:21:37 -0700

    Slide the RGB sliders to change the background color.

--- Colorboard/Colorboard/BNRColorViewController.m
+++ Colorboard/Colorboard/BNRColorViewController.m
@@ -10,6 +10,12 @@
 
 @interface BNRColorViewController ()
 
+@property (nonatomic, weak) IBOutlet UITextField *textField;
+
+@property (nonatomic, weak) IBOutlet UISlider *redSlider;
+@property (nonatomic, weak) IBOutlet UISlider *greenSlider;
+@property (nonatomic, weak) IBOutlet UISlider *blueSlider;
+
 @end
 
 @implementation BNRColorViewController
@@ -26,6 +32,17 @@
 
 - (IBAction)dismiss:(id)sender {
     [self.presentingViewController dismissViewControllerAnimated:YES completion:NULL];
+}
+
+- (IBAction)changeColoer:(id)sender {
+    float red = self.redSlider.value;
+    float green = self.greenSlider.value;
+    float blue = self.blueSlider.value;
+    UIColor *newColor = [UIColor colorWithRed:red
+                                        green:green
+                                         blue:blue
+                                        alpha:1.0];
+    self.view.backgroundColor = newColor;
 }
 
 /*
