e54a4c23082f2b6bc1171e0e0ce98f0b73e9c02f
Di Tian <peacefultd@gmail.com>
2016-07-12 13:56:04 -0700

    A glance of JavaScriptCore...

---
+++ Showtime-Starter/Native/Showtime/Showtime/Data/MovieService.swift
@@ -0,0 +1,98 @@
+/*
+* Copyright (c) 2015 Razeware LLC
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
+
+import UIKit
+import JavaScriptCore
+
+let movieUrl = "https://itunes.apple.com/us/rss/topmovies/limit=50/json"
+
+class MovieService {
+  
+  func loadMoviesWithLimit(limit: Double, onComplete complete:[Movie] -> ()) {
+    guard let url = NSURL(string: movieUrl) else {
+        print("Invalid url format: \(movieUrl)")
+        return
+    }
+    
+    NSURLSession.sharedSession().dataTaskWithURL(url) { data, _, _ in
+        guard let data = data,
+            jsonString = String(data: data, encoding: NSUTF8StringEncoding) else {
+                print("Error while parsing the response data.")
+                return
+        }
+        
+        let movies = self.parseResponse(jsonString, withLimit: limit)
+        complete(movies)
+    }.resume()
+  }
+  
+  func parseResponse(response: String, withLimit limit: Double) -> [Movie] {
+    // 1
+    guard let context = context else {
+      print("JSContext not found.")
+      return []
+    }
+    
+    // 2
+    let parseFunction = context.objectForKeyedSubscript("parseJson")
+    let parsed = parseFunction.callWithArguments([response]).toArray()
+    
+    // 3
+    let filterFunction = context.objectForKeyedSubscript("filterByLimit")
+    let filtered = filterFunction.callWithArguments([parsed, limit]).toArray()
+    
+    let mapFunction = context.objectForKeyedSubscript("mapToNative")
+    guard let unwrappedFiltered = filtered,
+      movies = mapFunction.callWithArguments([unwrappedFiltered]).toArray() as? [Movie] else {
+      print("Error while processing movies.")
+      return []
+    }
+    
+    return movies
+  }
+    
+  lazy var context: JSContext? = {
+    let context = JSContext()
+    
+    // 1
+    guard let
+        commonJSPath = NSBundle.mainBundle().pathForResource("common", ofType: "js"),
+        additionsJSPath = NSBundle.mainBundle().pathForResource("additions", ofType: "js") else {
+      print("Unable to read resource files.")
+      return nil
+    }
+    
+    // 2
+    do {
+      let common = try String(contentsOfFile: commonJSPath, encoding: NSUTF8StringEncoding)
+      let additions = try String(contentsOfFile: additionsJSPath, encoding: NSUTF8StringEncoding)
+      
+      context.setObject(Movie.self, forKeyedSubscript: "Movie")
+      context.evaluateScript(common)
+      context.evaluateScript(additions)
+    } catch (let error) {
+      print("Error while processing script file: \(error)")
+    }
+    
+    return context
+  }()
+}
