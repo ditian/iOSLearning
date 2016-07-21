63b4de145602b62d119c82f2f8924ed1ceecc7bd
Di Tian <peacefultd@gmail.com>
2016-07-21 16:36:57 -0700

    :gem:Initial commit for UICollectionViewExperiment:gem:

---
+++ UICollectionViewExperiment/UICollectionViewExperiment/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UICollectionViewExperiment
+//
+//  Created by Tian, Di on 7/21/16.
+//  Copyright © 2016 Tian, Di. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UICollectionViewExperiment/UICollectionViewExperiment/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UICollectionViewExperiment
+//
+//  Created by Tian, Di on 7/21/16.
+//  Copyright © 2016 Tian, Di. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
