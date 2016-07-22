ec3595078e0131fef432af56155379f7a6d6d88a
Di Tian <peacefultd@gmail.com>
2016-07-21 23:56:00 -0700

    Horizontal scroll cell prototype

--- UICollectionViewExperiment/UICollectionViewExperiment/AppDelegate.m
+++ UICollectionViewExperiment/UICollectionViewExperiment/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -16,7 +17,11 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    ViewController *rvc = [[ViewController alloc] init];
+    self.window.rootViewController = rvc;
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
     return YES;
 }
 
--- UICollectionViewExperiment/UICollectionViewExperiment/ViewController.m
+++ UICollectionViewExperiment/UICollectionViewExperiment/ViewController.m
@@ -10,18 +10,58 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) IBOutlet UICollectionView *collectionView;
+@property (nonatomic, strong) NSArray *dataArray;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSMutableArray *firstSection = [[NSMutableArray alloc] init];
+    for (int i = 0; i < 50; ++i) {
+        [firstSection addObject:[NSString stringWithFormat:@"Cell %d", i]];
+    }
+    self.dataArray = [[NSArray alloc] initWithObjects:firstSection, nil];
+    
+    UINib *cellNib = [UINib nibWithNibName:@"NibCell" bundle:nil];
+    [self.collectionView registerNib:cellNib forCellWithReuseIdentifier:@"cvCell"];
+    
+    UICollectionViewFlowLayout *flowLayout = [[UICollectionViewFlowLayout alloc] init];
+    [flowLayout setItemSize:CGSizeMake(160, 160)];
+    [flowLayout setScrollDirection:UICollectionViewScrollDirectionHorizontal];
+    [flowLayout setMinimumLineSpacing:0.0f];
+    [flowLayout setMinimumInteritemSpacing:0.0f];
+    
+    [self.collectionView setCollectionViewLayout:flowLayout];
+    
+    [self.collectionView setShowsVerticalScrollIndicator:NO];
+    [self.collectionView setShowsHorizontalScrollIndicator:NO];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
+    return [self.dataArray count];
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView
+     numberOfItemsInSection:(NSInteger)section {
+    NSMutableArray *sectionArray = [self.dataArray objectAtIndex:section];
+    return [sectionArray count];
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView
+                  cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    NSMutableArray *data = [self.dataArray objectAtIndex:indexPath.section];
+    NSString *cellData = [data objectAtIndex:indexPath.row];
+    static NSString *cellIdentifier = @"cvCell";
+    UICollectionViewCell *cell = [collectionView
+                                  dequeueReusableCellWithReuseIdentifier:cellIdentifier
+                                                            forIndexPath:indexPath];
+    UILabel *titleLabel = (UILabel *)[cell viewWithTag:100];
+    [titleLabel setText:cellData];
+    return cell;
 }
 
 @end
